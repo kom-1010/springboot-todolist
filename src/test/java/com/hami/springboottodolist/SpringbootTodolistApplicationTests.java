b0545841fe9c54e9e691089d36a2603d1e8fbb10
kom-1010 <kom-1010@naver.com>
2021-09-18 13:36:02 +0900

    Initial commit

---
+++ src/test/java/com/hami/springboottodolist/SpringbootTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hami.springboottodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
